e42693e79dfb00d5476f2b72dace78f22cef67aa
dz33 <derekzhao33@gmail.com>
2025-11-23 08:40:33 -0800

    fix: fix api routes

--- backend/src/shared/prisma.ts
+++ backend/src/shared/prisma.ts
@@ -1,9 +1,11 @@
+import 'dotenv/config';
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString: string = `${process.env.DATABASE_URL}`
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) throw new Error('DATABASE_URL missing');
 
-const adapter: PrismaPg = new PrismaPg({connectionString});
-const prisma: PrismaClient = new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
